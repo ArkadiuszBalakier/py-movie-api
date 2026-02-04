0f58c7c892e03d9b5410765b2806944938a4e15f
ArkadiuszBalakier <arkadiusz.balakier@gmail.com>
2026-02-04 11:31:15 +0100

    feat: add url for movie_list fix:  change serializers.ModelSerializer for serializers.Serializer, modify path name, add rest framework to installed app

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import Movie
 
 
-class MovieSerializer(serializers.ModelSerializer):
+class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField()
     description = serializers.CharField()
